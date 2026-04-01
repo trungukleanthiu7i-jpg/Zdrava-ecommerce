bcfb4251edc9a2b97bc0e485791e251424b16e41
Kleanthi Trungugit config --global user.email trungu.kleanthi.u7i@student.ucv.ro <trungu.kleanthi.u7i@student.ucv.ro>
2026-04-01 07:18:06 -0700

    Add userRoutes

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -6,12 +6,21 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 export async function sendEmail({ to, subject, html, text }) {
   if (!to) {
     throw new Error("Missing recipient email.");
   }
+
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+    throw new Error("EMAIL_USER or EMAIL_PASS is missing in environment variables.");
+  }
+
+  await transporter.verify();
 
   const info = await transporter.sendMail({
     from: process.env.EMAIL_FROM || process.env.EMAIL_USER,
